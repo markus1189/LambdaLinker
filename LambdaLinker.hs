179821bb55a5416e875b65a7be6326198d29ec0d
Markus Hauck <markus1189@gmail.com>
2012-08-24 15:41:30 +0200

    Added functions to list and link files

--- LambdaLinker.hs
+++ LambdaLinker.hs
@@ -1,15 +1,74 @@
 module LambdaLinker
   where
 
-import Control.Monad (liftM,forM)
+import Control.Monad ( liftM
+                     ,forM
+                     )
 
 import qualified System.Directory as S
-import System.Directory (doesDirectoryExist)
 
-import System.FilePath ((</>),addTrailingPathSeparator)
-import Data.List (isSuffixOf)
+import System.Directory ( doesDirectoryExist
+                        , createDirectoryIfMissing
+                        , getCurrentDirectory
+                        )
+
+import System.FilePath ( (</>)
+                       , addTrailingPathSeparator
+                       , dropExtension
+                       , takeDirectory
+                       )
+
+import System.Posix.Files ( createSymbolicLink )
+
+import Data.List (isSuffixOf,sort)
+
+main :: IO ()
+main = linkRelativeFilesToHomeDir
 
 type Predicate = FilePath -> Bool
+
+linkRelativeFilesToHomeDir :: IO ()
+linkRelativeFilesToHomeDir = do
+  curDir <- getCurrentDirectory
+  linkFiles (addTrailingPathSeparator curDir) "~/Desktop/fake_home/"
+
+linkFiles :: FilePath -> FilePath -> IO ()
+linkFiles from to = do
+  fromE <- expandPath from
+  toE <- expandPath to
+  files <- getFilesToLink fromE
+  let args = files `zip` (buildDestinations fromE toE files)
+  mapM_ (uncurry linkDotfile) args
+
+linkDotfile :: FilePath -> FilePath -> IO ()
+linkDotfile from to = do
+  putStrLn $ "Link: " ++ from ++ " -> " ++ to
+  createDirectoryIfMissing True (takeDirectory to)
+  createSymbolicLink from to
+
+buildDestinations :: FilePath -> FilePath -> [FilePath] -> [FilePath]
+buildDestinations from to = map $ buildDestination from to
+
+buildDestination :: FilePath -> FilePath -> FilePath -> FilePath
+buildDestination from to path = dropExtension $ to </> (drop (length from) path)
+
+getFilesToLink :: FilePath -> IO [FilePath]
+getFilesToLink path = sort `liftM` (filterSyms $ getRecursiveContentsP' path)
+  where filterSyms = liftM $ filter extensionIsSymlink
+        getRecursiveContentsP' =
+          getRecursiveContentsP (extensionIsSymlink `neitherP` gitDir)
+
+getRecursiveContentsP :: Predicate -> FilePath -> IO [FilePath]
+getRecursiveContentsP p root = do
+  root' <- expandPath root
+  contents <- getDirectoryContents root
+  liftM concat $ forM contents $ \name -> do
+    let path = root' </> name
+    isDirectory <- doesDirectoryExist path
+    if isDirectory && (doRecurse name)
+      then getRecursiveContentsP p path
+      else return [path]
+  where doRecurse = p
 
 getDirectoryContents :: FilePath -> IO [FilePath]
 getDirectoryContents path = filterD `liftM` (exPath >>= S.getDirectoryContents)
@@ -17,6 +76,7 @@ getDirectoryContents path = filterD `liftM` (exPath >>= S.getDirectoryContents)
         exPath = expandPath path
 
 expandPath :: FilePath -> IO FilePath
+expandPath "" = return ""
 expandPath (x:xs)
   | x == '~' = do
     home <- S.getHomeDirectory
@@ -28,8 +88,6 @@ extensionIsSymlink = (".symlink" `isSuffixOf`)
 
 gitDir :: Predicate
 gitDir = (".git/" `isSuffixOf`) . addTrailingPathSeparator
-
-predicate = neitherP gitDir extensionIsSymlink
 
 notP :: Predicate -> Predicate
 notP p path = not $ p path
